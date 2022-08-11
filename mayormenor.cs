ed6eb9490d4182f8f360e4aecfa16595fc0ddc30
Isabelagiraldoj <110170428+Isabelagiraldoj@users.noreply.github.com>
2022-08-10 23:50:24 -0500

    Add files via upload

---
+++ mayormenor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeromayor
+{
+    // Pedir dos números al usuario por teclado y decir que número es el mayor.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Escribeme el primer numero");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escribeme el segundo numero");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            //ToInt32 dara 0
+            if (num1 >= num2)
+            {
+                Console.WriteLine("El primer numero es mayor o igual que el segundo");
+            }
+            else
+            {
+                Console.WriteLine("El segundo numero es mayor que el primero");
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ numerosdel1al100.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_del_1_al_100
+{
+    // escribir un programa que genere los dias del 1 al 100
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for se usa para ejecutar condiciones
+            // i y j son el rango de valores ue se tomaran, se pone ++ si es el siguiente o -- si es el anterior
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
