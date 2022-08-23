db45d57eafb884014566615331de89b206e2fd24
Isabelagiraldoj <110170428+Isabelagiraldoj@users.noreply.github.com>
2022-08-23 06:58:55 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplosparametrosfunciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para demostrar los tipos de parametros en funciones");
+
+            Console.Write("Ingresa un numero entero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"El numero antes de entrar a la funcion por valor es {numero}");
+            IncrementaValorNumero(numero);
+
+            IncrementaReferenciaNumero(ref numero);
+            Console.WriteLine($"el numero despues de entarar a la funcion es {numero} ");
+
+            Console.Read();
+
+
+            
+        }
+        //si estan en llaves distintas son contextos diferentes
+
+        static void IncrementaReferenciaNumero (ref int numero)
+        {
+            numero  += 5;
+            Console.WriteLine($"El numero en la funcion es {numero}");
+        }
+        static void IncrementaValorNumero(int datoNumero)
+        {
+            datoNumero +=5;
+            Console.WriteLine($"El numero en la funcion es {datoNumero}");
+
+        }
+
+        
+    }
+}
